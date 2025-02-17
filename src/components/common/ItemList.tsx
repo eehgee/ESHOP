769389a68ab9a0cad1ed045fe2e1db221863c206
kim eun ju <dfiow@naver.com>
2025-02-17 17:10:21 +0900

    price filtering commit

--- src/components/common/ItemList.tsx
+++ src/components/common/ItemList.tsx
@@ -12,6 +12,8 @@ interface ItemCategoryProps {
 const ItemList = ({ category, title }: ItemCategoryProps): JSX.Element => {
   const loaddata = useRecoilValueLoadable(productsItem);
 
+  const [sortOrder, setSortOrder] = useState<"asc" | "desc" | null>(null);
+
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 8; // 페이지당 8개 아이템 보여줌
 
@@ -48,20 +50,48 @@ const ItemList = ({ category, title }: ItemCategoryProps): JSX.Element => {
       category.includes(item.category)
     );
 
+    const sortedItems = sortOrder
+      ? [...filteredItems].sort((a, b) =>
+          sortOrder === "asc" ? a.price - b.price : b.price - a.price
+        )
+      : filteredItems;
+
     // 전체 페이지 수 계산
     const totalPages = Math.ceil(filteredItems.length / itemsPerPage);
 
     // 현재 페이지에 해당하는 아이템만 선택
     const startIndex = (currentPage - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    const currentItems = filteredItems.slice(startIndex, endIndex);
+    const currentItems = sortedItems.slice(startIndex, endIndex);
 
     return (
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 lg:max-w-7xl lg:px-8 mt-36">
-          <h2 className="text-3xl md:text-4xl text-center m-10">
-            {title}
-          </h2>
+          <h2 className="text-3xl md:text-4xl text-center m-10">{title}</h2>
+          <div className="flex justify-end mb-4">
+            <button
+              className={`px-2 py-2 text-black text-sm ${
+                sortOrder === "asc"
+                  ? "underline decoration-black"
+                  : sortOrder === null
+              }`}
+              onClick={() => setSortOrder("asc")}
+            >
+              낮은 가격순
+            </button>
+            <span className="text-gray-400 px-2 py-2">|</span>
+            <button
+              className={`px-2 py-2 text-black text-sm ${
+                sortOrder === "desc"
+                  ? "underline decoration-black"
+                  : sortOrder === null
+              }`}
+              onClick={() => setSortOrder("desc")}
+            >
+              높은 가격순
+            </button>
+          </div>
+
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {currentItems.map((item) => (
               <Link to={`/item/${item.id}`} key={item.id} className="group">
