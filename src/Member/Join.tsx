98659ea7cfb74310cd9efeaf53e3564e70b83a9d
kim eun ju <dfiow@naver.com>
2025-02-19 15:18:41 +0900

    join commit

--- src/Member/Join.tsx
+++ src/Member/Join.tsx
@@ -1,6 +1,51 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Join = (): JSX.Element => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    verifyPassword: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    if (formData.password !== formData.verifyPassword) {
+      setError("비밀번호가 일치하지 안허습니다.");
+      return;
+    }
+
+    const users = JSON.parse(localStorage.getItem("users") || "[]");
+
+    const isEmail = users.some((user: any) => user.email === formData.email);
+    if (isEmail) {
+      setError("이미 가입된 이메일입니다.");
+      return;
+    }
+
+    const newUser = {
+      name: formData.name,
+      email: formData.email,
+      password: formData.password,
+    };
+
+    localStorage.setItem("users", JSON.stringify([...users, newUser]));
+
+    alert("가입이 완료되었습니다!");
+    navigate("/login");
+  };
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-20 lg:px-8 mt-36">
@@ -11,77 +56,69 @@ const Join = (): JSX.Element => {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
+          <form onSubmit={handleSubmit} className="space-y-6">
             <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="name" className="block text-sm/6">
-                  이름
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full border-b border-gray-400 focus:border-black bg-white px-3 py-1.5 text-base placeholder:text-gray-400 sm:text-sm/6"
-                />
-              </div>
+              <label htmlFor="name" className="block text-sm">
+                이름
+              </label>
+
+              <input
+                id="name"
+                name="name"
+                type="text"
+                required
+                value={formData.name}
+                onChange={handleChange}
+                className="block w-full border-b border-gray-400 focus:border-black bg-white px-3 py-1.5 text-base placeholder:text-gray-400 sm:text-sm/6"
+              />
             </div>
 
             <div>
-              <label htmlFor="email" className="block text-sm/6">
+              <label htmlFor="email" className="block text-sm">
                 이메일 주소(아이디)
               </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  className="block w-full border-b border-gray-400 focus:border-black bg-white px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 sm:text-sm/6"
-                />
-              </div>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                value={formData.email}
+                onChange={handleChange}
+                className="block w-full border-b border-gray-400 focus:border-black bg-white px-3 py-1.5 text-base text-gray-900 placeholder:text-gray-400 sm:text-sm/6"
+              />
             </div>
 
             <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm/6">
-                  비밀번호
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full border-b border-gray-400 focus:border-black bg-white px-3 py-1.5 text-base placeholder:text-gray-400 sm:text-sm/6"
-                />
-              </div>
+              <label htmlFor="password" className="block text-sm">
+                비밀번호
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                value={formData.password}
+                onChange={handleChange}
+                className="block w-full border-b border-gray-400 focus:border-black bg-white px-3 py-1.5 text-base placeholder:text-gray-400 sm:text-sm/6"
+              />
             </div>
 
             <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="verifypassword" className="block text-sm/6">
-                  비밀번호 확인
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="verifypassword"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full border-b border-gray-400 focus:border-black bg-white px-3 py-1.5 text-base placeholder:text-gray-400 sm:text-sm/6"
-                />
-              </div>
-            </div>
+              <label htmlFor="verifyPassword" className="block text-sm">
+                비밀번호 확인
+              </label>
 
+              <input
+                id="verifyPassword"
+                name="verifyPassword"
+                type="password"
+                required
+                value={formData.verifyPassword}
+                onChange={handleChange}
+                className="block w-full border-b border-gray-400 focus:border-black bg-white px-3 py-1.5 text-base placeholder:text-gray-400 sm:text-sm/6 "
+              />
+            </div>
+            {error && <p className="text-red-500 text-sm">{error}</p>}
             <div>
               <button
                 type="submit"
